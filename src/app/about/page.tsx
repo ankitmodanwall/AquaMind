411e16d48e18d0779fcd1f16dac1506b0cf14ce3
Ankit Modanwal <ankitmd001@gmail.com>
2025-09-15 17:40:59 +0000

    Automatically created for pushes to ankitmodanwall/AquaMind
    Upgrade t

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Droplets, GaugeCircle, BookOpen, MessagesSquare, Info, Star } from "lucide-react";
+import { Droplets, GaugeCircle, BookOpen, MessagesSquare, Info } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
